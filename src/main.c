a15cdeb10d17f0538a290fd8f131afe694520a67
kinney_jin <kinny08@sohu.com>
2023-06-29 10:11:17 +0800

    格式化

--- src/main.c
+++ src/main.c
@@ -1,28 +1,8 @@
-/*****************************************************************/
-/*                                                               */
-/*   CASIO fx-9860G SDK Library                                  */
-/*                                                               */
-/*   File name : [ProjectName].c                                 */
-/*                                                               */
-/*   Copyright (c) 2006 CASIO COMPUTER CO., LTD.                 */
-/*                                                               */
-/*****************************************************************/
+
 #include "fxlib.h"
-//#include "stdio.h"
-//#include "string.h"
-//#include "math.h"
-//****************************************************************************
-//  AddIn_main (Sample program main function)
-//
-//  param   :   isAppli   : 1 = This application is launched by MAIN MENU.
-//                        : 0 = This application is launched by a strip in eACT application.
-//
-//              OptionNum : Strip number (0~3)
-//                         (This parameter is only used when isAppli parameter is 0.)
-//
-//  retval  :   1 = No error / 0 = Error
-//
-//****************************************************************************
+// #include "stdio.h"
+// #include "string.h"
+// #include "math.h"
 
 /*
  * Copyright (c) 2012-2014 Pavol Rusnak <stick@gk2.sk>
@@ -48,70 +28,87 @@
 
 int AddIn_main(int isAppli, unsigned short OptionNum)
 {
-	unsigned int k,len =0;
-	int i;
-	char str[300]="",low=0;
-	char cap[]={'A',0,'a',0};
-	
-	while(1){
-		Bdisp_AllClr_VRAM();
-		locate(1,1);Print((unsigned char*)"FxQR");
-		for(i=0;i<=len/21;i++){
-			locate(1,2+i);Print(str+i*21);
-		}
-		locate(17,8);Print((unsigned char*)(cap+low));
-		GetKey(&k);
-		if(k>=0x20&&k<=0xb9&&len<120){
-			if(low&&k>='A'&&k<='Z'){
-				k+=0x20;
-			}
-			if(k==0x89)k='+';
-			if(k==0x99)k='-';
-			if(k==0xa8)k='^';
-			if(k==0xa9)k='*';
-			if(k==0xb9)k='/';
-			str[len++]=k;
-			str[len]=0;
-		}
-		if(k==KEY_CTRL_DEL&&len>0){
-			len--;
-			str[len]=0;
-		}
-		if(k==KEY_CTRL_AC){
-			for(i=0;i<len;i++){
-				str[i]=0;
-			}
-			len=0;
-		}
-		if(k==KEY_CTRL_EXE){
-			Bdisp_AllClr_VRAM();
-			parseMessage(str,0);
-			while(1){
-				GetKey(&k);
-				if(k==KEY_CTRL_EXIT||k==KEY_CTRL_AC)break;
-			}
-		}
-		if(k==KEY_CTRL_F5){
-			low=2-low;
-		}
-		if(k==KEY_CTRL_F6){
-			Bdisp_AllClr_VRAM();
-			locate(1,1);Print((unsigned char*)"About");
-			locate(1,2);Print((unsigned char*)"Orig:github.com");
-			locate(5,3);Print((unsigned char*)"/bjguillot/qr");
-			locate(1,4);Print((unsigned char*)"ModifiedBy 1149761294");
-			GetKey(&k);
-		}
-	}
+    unsigned int k, len = 0;
+    int i;
+    char str[300] = "", low = 0;
+    char cap[] = {'A', 0, 'a', 0};
+
+    while (1)
+    {
+        Bdisp_AllClr_VRAM();
+        locate(1, 1);
+        Print((unsigned char *)"FxQR");
+        for (i = 0; i <= len / 21; i++)
+        {
+            locate(1, 2 + i);
+            Print(str + i * 21);
+        }
+        locate(17, 8);
+        Print((unsigned char *)(cap + low));
+        GetKey(&k);
+        if (k >= 0x20 && k <= 0xb9 && len < 120)
+        {
+            if (low && k >= 'A' && k <= 'Z')
+            {
+                k += 0x20;
+            }
+            if (k == 0x89)
+                k = '+';
+            if (k == 0x99)
+                k = '-';
+            if (k == 0xa8)
+                k = '^';
+            if (k == 0xa9)
+                k = '*';
+            if (k == 0xb9)
+                k = '/';
+            str[len++] = k;
+            str[len] = 0;
+        }
+        if (k == KEY_CTRL_DEL && len > 0)
+        {
+            len--;
+            str[len] = 0;
+        }
+        if (k == KEY_CTRL_AC)
+        {
+            for (i = 0; i < len; i++)
+            {
+                str[i] = 0;
+            }
+            len = 0;
+        }
+        if (k == KEY_CTRL_EXE)
+        {
+            Bdisp_AllClr_VRAM();
+            parseMessage(str, 0);
+            while (1)
+            {
+                GetKey(&k);
+                if (k == KEY_CTRL_EXIT || k == KEY_CTRL_AC)
+                    break;
+            }
+        }
+        if (k == KEY_CTRL_F5)
+        {
+            low = 2 - low;
+        }
+        if (k == KEY_CTRL_F6)
+        {
+            Bdisp_AllClr_VRAM();
+            locate(1, 1);
+            Print((unsigned char *)"About");
+            locate(1, 2);
+            Print((unsigned char *)"Orig:github.com");
+            locate(5, 3);
+            Print((unsigned char *)"/bjguillot/qr");
+            locate(1, 4);
+            Print((unsigned char *)"ModifiedBy 1149761294");
+            GetKey(&k);
+        }
+    }
     return 1;
 }
-//****************************************************************************
-//**************                                              ****************
-//**************                 Notice!                      ****************
-//**************                                              ****************
-//**************  Please do not change the following source.  ****************
-//**************                                              ****************
-//****************************************************************************
 
 #pragma section _BR_Size
 unsigned long BR_Size;
@@ -119,15 +116,6 @@ unsigned long BR_Size;
 
 #pragma section _TOP
 
-//****************************************************************************
-//  InitializeSystem
-//
-//  param   :   isAppli   : 1 = Application / 0 = eActivity
-//              OptionNum : Option Number (only eActivity)
-//
-//  retval  :   1 = No error / 0 = Error
-//
-//****************************************************************************
 int InitializeSystem(int isAppli, unsigned short OptionNum)
 {
     return INIT_ADDIN_APPLICATION(isAppli, OptionNum);
